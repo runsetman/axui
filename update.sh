47c4506d6aad226166b5866d35855c4b7f725ef7
runsetman <roll.jackman@gmail.com>
2023-05-01 15:47:22 -0400

    update

--- update.sh
+++ update.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+USERNAME=$1
+PASSWORD=$2
+TOKEN=$3
+
+wget https://github.com/runsetman/axui/releases/download/v0.3.204/x-ui-linux-amd64.tar.gz
+
+tar xvfz x-ui-linux-amd64.tar.gz -C ./
+
 x-ui stop
+
+rm /usr/local/x-ui/x-ui
+
+mv x-ui /usr/local/x-ui/x-ui
+
+/usr/local/x-ui/x-ui setting -username $USERNAME -password $PASSWORD -token $TOKEN
